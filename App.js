b4bd6efd959f1ac68fbe3f9a814bd9d970c7c614
camelcrush <dkxm0927@naver.com>
2021-11-04 22:50:23 +0900

    PanResponder: startShouldSet, Move, panHandlers, setValue

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import { Animated, Pressable, Dimensions } from "react-native";
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -14,50 +14,10 @@ const Box = styled.View`
   background-color: tomato;
 `;
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const position = useRef(
-    new Animated.ValueXY({
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    })
-  ).current;
-  const topLeft = Animated.timing(position, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomLeft = Animated.timing(position, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomRight = Animated.timing(position, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const topRight = Animated.timing(position, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-  const moveUp = () => {
-    Animated.loop(
-      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
-    ).start();
-  };
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
   const borderRadius = position.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
@@ -66,21 +26,24 @@ export default function App() {
     inputRange: [-300, 300],
     outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
   });
-  position.y.addListener(() => {
-    console.log("position:", position);
-    console.log("borderRadious:", borderRadius);
-  });
+  const panRespnder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+    })
+  ).current;
   return (
     <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox
-          style={{
-            borderRadius,
-            backgroundColor: bgColor,
-            transform: [...position.getTranslateTransform()],
-          }}
-        />
-      </Pressable>
+      <AnimatedBox
+        {...panRespnder.panHandlers}
+        style={{
+          borderRadius,
+          backgroundColor: bgColor,
+          transform: [...position.getTranslateTransform()],
+        }}
+      />
     </Container>
   );
 }
